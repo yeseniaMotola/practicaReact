fdb69b4a9847f91c7fee1a83241316c29878a543
Yesenia Motola <yeseniamotola@gmail.com>
2022-10-25 00:07:11 -0300

    components

--- miau-store/src/App.jsx
+++ miau-store/src/App.jsx
@@ -1,22 +1,13 @@
 
 import './App.css';
+import Navbar from './components/Navbar.js';
 
-function App() {
+
+function App () {
   return (
-    <header className='header'>
-      <div className='logo'>
-        <h1>miau <span>store</span></h1>
-      </div>
-      <nav>
-        <ul className='navLinks'>
-          <li>alimentos</li>
-          <li>objetos</li>
-          <li>plantas</li>
-          <li>contacto</li>
-        </ul>
-      </nav>
-      <button className='botonBuscar'>buscar</button>
-    </header>
+<div>
+  <Navbar/>
+</div>
   );
 }
 
---
+++ miau-store/src/components/Navbar.js
@@ -0,0 +1,36 @@
+
+const Navbar = () => {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">miau-store</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor02">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">home
+                  <span className="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">products</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">about</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-sm-2" type="text" placeholder="Search" />
+              <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+  export default Navbar;
